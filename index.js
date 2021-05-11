ee1e9a95c90de6b64bfa814307c600e03f370026
name <fimmup@gmail.com>
2021-05-11 14:28:01 +0900

    　2回目　

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key')
+
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seprole:wltnwkd11@nodejs.el0rj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(() => console.log('mongoDB Connected..'))
   .catch(err => console.log(err))
@@ -12,8 +21,25 @@ mongoose.connect('mongodb+srv://seprole:wltnwkd11@nodejs.el0rj.mongodb.net/myFir
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!!')
+  res.send('Hello World!!')
 })
+
+
+app.post('/register', (req, res) => {
+
+  //サインアップする時必要な情報をデータベースに格納する
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
